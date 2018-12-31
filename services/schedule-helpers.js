041db5dfbfb83246c706a2e975c941a5ee700603
Ryan <ryanvolum@users.noreply.github.com>
2018-12-31 12:30:42 -0800

    renamed and updated helpers

---
+++ services/schedule-helpers.js
@@ -0,0 +1,84 @@
+const schedule = require('../data/foodBankSchedule.json');
+const { Attachment, CardFactory, MessageFactory } = require("botbuilder");
+
+// ================== DONATION HELPERS =======================
+const getValidDonationDays = () => {
+    let days = [];
+    schedule.forEach(foodBank => {
+        foodBank.donationHours.forEach(t => {
+            if (!days.includes(t.day)) {
+                days.push(t.day);
+            }
+        })
+    })
+    return days;
+}
+
+const filterFoodBanksByDonation = (day) => {
+    return schedule.filter(foodBank => foodBankDonationIsAvailable(foodBank, day));
+}
+
+const foodBankDonationIsAvailable = (foodBank, day) => {
+    for (let i = 0; i < foodBank.donationHours.length; i++) {
+        if (foodBank.donationHours[i].day === day)
+            return true;
+    }
+}
+
+const createFoodBankDonationCardAttachment = (foodBank) => {
+    let allHours = foodBank.donationHours.reduce((prev, curr) => prev += `\r${curr.day} ${curr.hours}`, "");
+
+    let cardText = `**${foodBank.name}**\r${foodBank.donationNotes}\r**Hours**${allHours}`;
+
+    return CardFactory.heroCard(cardText);
+}
+
+const createFoodBankDonationCarousel = (foodBanks) => {
+    const attachments = foodBanks.map((foodBank) => createFoodBankDonationCardAttachment(foodBank));
+    return (MessageFactory.carousel(attachments));
+}
+
+// ================== PICKUP HELPERS =======================
+
+const getValidPickupDays = () => {
+    let days = [];
+    schedule.forEach(foodBank => {
+        foodBank.pickupHours.forEach(t => {
+            if (!days.includes(t.day)) {
+                days.push(t.day);
+            }
+        })
+    })
+    return days;
+}
+
+const filterFoodBanksByPickup = (day) => {
+    return schedule.filter(foodBank => foodBankPickupIsAvailable(foodBank, day));
+}
+
+const foodBankPickupIsAvailable = (foodBank, day) => {
+    for (let i = 0; i < foodBank.pickupHours.length; i++) {
+        if (foodBank.pickupHours[i].day === day)
+            return true;
+    }
+}
+
+const createFoodBankPickupCardAttachment = (foodBank) => {
+    let allHours = foodBank.pickupHours.reduce((prev, curr) => prev += `\r${curr.day} ${curr.hours}`, "");
+
+    let cardText = `**${foodBank.name}**\r${foodBank.pickupNotes}\r**Hours**${allHours}`;
+
+    return CardFactory.heroCard(cardText);
+}
+
+const createFoodBankPickupCarousel = (foodBanks) => {
+    const attachments = foodBanks.map((foodBank) => createFoodBankPickupCardAttachment(foodBank));
+    return (MessageFactory.carousel(attachments));
+}
+
+module.exports.filterFoodBanksByDonation = filterFoodBanksByDonation;
+module.exports.filterFoodBanksByPickup = filterFoodBanksByPickup;
+module.exports.getValidDonationDays = getValidDonationDays;
+module.exports.getValidPickupDays = getValidPickupDays;
+module.exports.createFoodBankDonationCarousel = createFoodBankDonationCarousel;
+module.exports.createFoodBankPickupCarousel = createFoodBankPickupCarousel;
